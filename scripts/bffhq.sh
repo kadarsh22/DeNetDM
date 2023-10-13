b7b03dada4163708660e9dd01bba21b5c3232f2f
silpavs.43 <silpavs.43@gmail.com>
2023-10-13 15:12:56 +0100

    bffhq sript added

--- scripts/bffhq.sh
+++ scripts/bffhq.sh
@@ -3,7 +3,7 @@
 # get the sh file executable  chmod +x run.sh, run the sh file using nohup bash scripts/corruptedcifar.sh &
 
 
-seeds=(274 87)
+seeds=(274 87 5 5881 1)
 
 for seed in "${seeds[@]}"; do 
     echo "Running for seed ${seed}"
